adecbaa688e0db13f7e9ccbd816ab3ae04cc40af
Doomhack <gamefreaks@hotmail.com>
2020-01-23 21:51:09 +0000

    8x unroll on u32 division.
    Don't use ALL of the IWRAM! :-)

--- source/fixeddiv.s
+++ source/fixeddiv.s
@@ -41,26 +41,48 @@ udiv32_arm :
 
     mov r3, r1                   /* r3 ← r1 */
     cmp r3, r0, LSR #1           /* update cpsr with r3 - r0/2 */
-    .Lloop2:
-      movls r3, r3, LSL #1       /* if r3 <= 2*r0 (C=0 or Z=1) then r3 ← r3*2 */
-      cmp r3, r0, LSR #1         /* update cpsr with r3 - (r0/2) */
-      bls .Lloop2                /* branch to .Lloop2 if r3 <= 2*r0 (C=0 or Z=1) */
+
+.Lloop2:
+
+
+.rept 7
+        movls r3, r3, LSL #1       /* if r3 <= 2*r0 (C=0 or Z=1) then r3 ← r3*2 */
+        cmp r3, r0, LSR #1         /* update cpsr with r3 - (r0/2) */
+        bhi .L2Done                /* branch to .Lloop2 if r3 <= 2*r0 (C=0 or Z=1) */
+.endr
+
+        movls r3, r3, LSL #1       /* if r3 <= 2*r0 (C=0 or Z=1) then r3 ← r3*2 */
+        cmp r3, r0, LSR #1         /* update cpsr with r3 - (r0/2) */
+        bls .Lloop2                /* branch to .Lloop2 if r3 <= 2*r0 (C=0 or Z=1) */
+
+
+
+.L2Done:
 
     mov r2, #0                   /* r2 ← 0 */
 
-    .rept 31
+.Lloop3:
 
-      cmp r0, r3                 /* update cpsr with r0 - r3 */
-      subhs r0, r0, r3           /* if r0 >= r3 (C=1) then r0 ← r0 - r3 */
-      adc r2, r2, r2             /* r2 ← r2 + r2 + C.
+.rept 7
+        cmp r0, r3                 /* update cpsr with r0 - r3 */
+        subhs r0, r0, r3           /* if r0 >= r3 (C=1) then r0 ← r0 - r3 */
+        adc r2, r2, r2             /* r2 ← r2 + r2 + C.
                                     Note that if r0 >= r3 then C=1, C=0 otherwise */
 
-      mov r3, r3, LSR #1         /* r3 ← r3/2 */
-      cmp r3, r1                 /* update cpsr with r3 - r1 */
-      blt .Done                  /* if r3 < r1 branch to .Done */
+        mov r3, r3, LSR #1         /* r3 ← r3/2 */
+        cmp r3, r1                 /* update cpsr with r3 - r1 */
+        blt .L3Done                  /* if r3 < r1 branch to .Done */
+.endr
 
-    .endr
+        cmp r0, r3                 /* update cpsr with r0 - r3 */
+        subhs r0, r0, r3           /* if r0 >= r3 (C=1) then r0 ← r0 - r3 */
+        adc r2, r2, r2             /* r2 ← r2 + r2 + C.
+                                    Note that if r0 >= r3 then C=1, C=0 otherwise */
 
-   .Done:
+        mov r3, r3, LSR #1         /* r3 ← r3/2 */
+        cmp r3, r1                 /* update cpsr with r3 - r1 */
+        bhs .Lloop3                /* if r3 >= r1 branch to .Lloop3 */
+
+.L3Done:
     mov r0, r2                   /* move quotient to r0 return val */
     bx lr
