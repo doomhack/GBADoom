eb9262889255311c1b54134edfe1e530f232ba6a
Doomhack <zlaruebuckley@gmail.com>
2020-02-29 22:35:10 +0000

    Use shareware iwad by default.

--- source/doom_iwad.c
+++ source/doom_iwad.c
@@ -1,7 +1,7 @@
 #pragma GCC optimize ("-O0")
 
-//#include "iwad/doom1.c"
-#include "iwad/doomu.c"
+#include "iwad/doom1.c"
+//#include "iwad/doomu.c"
 //#include "iwad/doom2.c"
 //#include "iwad/tnt.c"
 //#include "iwad/plutonia.c"
