c9788aaf033eb7ee16b29b6242923ab1a841da2d
Kippykip <gm_kip@hotmail.com>
2020-06-02 20:48:20 +1000

    FPS Counter as a Cheat
    Make the existing doomhack FPS counter a cheat intead of a source edit everytime.
    A, B, L, Up, Down, B, Left, Left

--- source/d_main.c
+++ source/d_main.c
@@ -97,8 +97,6 @@ const boolean nodrawers = false;
 
 static const char* timedemo = NULL;//"demo1";
 
-static const boolean show_fps = false;
-
 /*
  * D_PostEvent - Event handling
  *
@@ -289,7 +287,7 @@ static void D_DoomLoop(void)
         D_Display();
 
 
-        if(show_fps)
+        if(_g->fps_show)
         {
             D_UpdateFPS();
         }
@@ -760,7 +758,7 @@ static void D_DoomMainSetup(void)
 
     _g->idmusnum = -1; //jff 3/17/98 insure idmus number is blank
 
-    _g->fps_show = show_fps;
+    _g->fps_show = false;
 
     I_InitGraphics();
 
