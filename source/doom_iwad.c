782011bb0897b48f49f1a6193efdf4bbc078ae51
Kippykip <gm_kip@hotmail.com>
2020-06-02 20:48:52 +1000

    Edit Doom IWAD comments
    Add the 3 episode Doom.wad to the commenting list.

--- source/doom_iwad.c
+++ source/doom_iwad.c
@@ -1,11 +1,12 @@
 #pragma GCC optimize ("-O0")
 
-//#include "iwad/doom1.c"
+#include "iwad/doom1.c"
+//#include "iwad/doom.c"
 //#include "iwad/doomu.c"
 //#include "iwad/doom2.c"
 //#include "iwad/tnt.c"
 //#include "iwad/plutonia.c"
-#include "iwad/sigil.c"
+//#include "iwad/sigil.c"
 
 #include "doom_iwad.h"
 
